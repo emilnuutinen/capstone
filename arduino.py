540eb5733fac610faa670d67fc6c8d3284066861
Emil Nuutinen <emil@emilnuutinen.com>
2021-11-03 15:17:30 +0200

    clean up the code and add .gitignore

--- arduino.py
+++ arduino.py
@@ -1,6 +1,7 @@
 import serial
 
-arduino_port = "/dev/cu.SLAB_USBtoUART"  # serial port of Arduino
+# serial port of Arduino ("COM5" in Windows)
+arduino_port = "/dev/cu.SLAB_USBtoUART"
 baud = 115200  # arduino uno runs at 115200 baud
 fileName = "sensor_data.csv"  # name of the CSV file generated
 
@@ -13,10 +14,10 @@ while line <= samples:
     getData = str(ser.readline())
     data = getData  # ecg_timestamp,ecg,ppg1_timestamp,ppg1_ir,ppg1_red,ppg1_green,ppg2_timestamp,ppg2_ir,ppg2_red,ppg2_green
     print(data)
-    if line > 8:
+    if line > 8:  # Ignore Arduino info in the beginning
         file = open(fileName, "a")
-        file.write(data[2:-5] + "\r\n")  # write data with a newline
+        file.write(data[2:-5] + "\r\n")  # Ignore extra characters
     line = line+1
 
-print("Data collection complete!")
+print("Finished")
 file.close()
