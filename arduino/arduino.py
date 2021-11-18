423cad97ced58150d18f8026cec94e80f135d89c
Emil Nuutinen <emil@emilnuutinen.com>
2021-11-18 17:07:01 +0200

    2 measurements from Carlos with ECG only

--- arduino/arduino.py
+++ arduino/arduino.py
@@ -8,7 +8,7 @@ baud = 115200  # arduino uno runs at 115200 baud
 
 # take datetime now in ISO 8061 format and make that the filename
 now = datetime.datetime.now()
-file_name = now.strftime("%Y-%m-%d-%H-%M-%S") + ".csv"
+file_name = now.strftime("%Y-%m-%d-%H-%M-%S") + "ECG_ONLY.csv"
 
 # Path for the data file
 data_folder = Path("data/raw/")
@@ -23,7 +23,7 @@ while line <= samples:
     get_data = str(ser.readline())
     data = get_data  # ecg_timestamp,ecg,ppg1_timestamp,ppg1_ir,ppg1_red,ppg1_green,ppg2_timestamp,ppg2_ir,ppg2_red,ppg2_green
     print(data)
-    if line > 10:  # Ignore Arduino info in the beginning
+    if line > 8:  # Ignore Arduino info in the beginning
         file = open(file_path, "a")
         file.write(data[2:-5] + "\r\n")  # Ignore extra characters
     line = line+1
