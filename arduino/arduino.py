42d04dadd0fc96a9d9faa3d4d978613e00b65874
Emil Nuutinen <emil@emilnuutinen.com>
2021-11-18 16:46:57 +0200

    3 measurements from Carlos with all 3 sensors

--- arduino/arduino.py
+++ arduino/arduino.py
@@ -10,20 +10,20 @@ baud = 115200  # arduino uno runs at 115200 baud
 now = datetime.datetime.now()
 file_name = now.strftime("%Y-%m-%d-%H-%M-%S") + ".csv"
 
-# Path  for the data file
+# Path for the data file
 data_folder = Path("data/raw/")
 file_path = data_folder / file_name
 
 ser = serial.Serial(arduino_port, baud)
 print("Connected to Arduino port:" + arduino_port)
 
-samples = 100  # how many samples to collect
+samples = 2000  # how many samples to collect
 line = 0
 while line <= samples:
     get_data = str(ser.readline())
     data = get_data  # ecg_timestamp,ecg,ppg1_timestamp,ppg1_ir,ppg1_red,ppg1_green,ppg2_timestamp,ppg2_ir,ppg2_red,ppg2_green
     print(data)
-    if line > 8:  # Ignore Arduino info in the beginning
+    if line > 10:  # Ignore Arduino info in the beginning
         file = open(file_path, "a")
         file.write(data[2:-5] + "\r\n")  # Ignore extra characters
     line = line+1
