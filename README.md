c48b5aaca1c5f50148dea28e4bdf169bd7043037
Emil Nuutinen <emil@emilnuutinen.com>
2021-11-18 08:45:06 +0200

    Initial folder structure

---
+++ README.md
@@ -0,0 +1,8 @@
+# Continuous non-invasice blood pressure monitoring
+
+### Components used
+
+- [Adafruit HUZZAH32 – ESP32 Feather Board](https://www.adafruit.com/product/3405)
+- [MAX30105 Particle and Pulse Ox Sensor(PPG)](https://www.sparkfun.com/products/16474)
+- [Gravity: Analog Heart Rate Monitor Sensor (ECG)](https://www.dfrobot.com/product-1510.html)
+
--- arduino/arduino.py
+++ arduino/arduino.py
@@ -1,9 +1,18 @@
 import serial
+import datetime
+from pathlib import Path
 
 # serial port of Arduino ("COM5" in Windows)
 arduino_port = "/dev/cu.SLAB_USBtoUART"
 baud = 115200  # arduino uno runs at 115200 baud
-fileName = "sensor_data.csv"  # name of the CSV file generated
+
+# take datetime now in ISO 8061 format and make that the filename
+now = datetime.datetime.now()
+file_name = now.strftime("%Y-%m-%d-%H-%M-%S") + ".csv"
+
+# Path  for the data file
+data_folder = Path("data/raw/")
+file_path = data_folder / file_name
 
 ser = serial.Serial(arduino_port, baud)
 print("Connected to Arduino port:" + arduino_port)
@@ -11,11 +20,11 @@ print("Connected to Arduino port:" + arduino_port)
 samples = 100  # how many samples to collect
 line = 0
 while line <= samples:
-    getData = str(ser.readline())
-    data = getData  # ecg_timestamp,ecg,ppg1_timestamp,ppg1_ir,ppg1_red,ppg1_green,ppg2_timestamp,ppg2_ir,ppg2_red,ppg2_green
+    get_data = str(ser.readline())
+    data = get_data  # ecg_timestamp,ecg,ppg1_timestamp,ppg1_ir,ppg1_red,ppg1_green,ppg2_timestamp,ppg2_ir,ppg2_red,ppg2_green
     print(data)
     if line > 8:  # Ignore Arduino info in the beginning
-        file = open(fileName, "a")
+        file = open(file_path, "a")
         file.write(data[2:-5] + "\r\n")  # Ignore extra characters
     line = line+1
 
