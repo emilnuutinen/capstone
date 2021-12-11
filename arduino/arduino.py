3b73ab2e87dd97fbb29f3c9c1e2b6c5fccac1992
Emil Nuutinen <emil@emilnuutinen.com>
2021-12-11 10:25:58 +0200

    Optimize data collection loop. Recognize OS and choose port path accordingly

--- arduino/arduino.py
+++ arduino/arduino.py
@@ -1,32 +1,44 @@
 import serial
 import datetime
+import re
+import platform
 from pathlib import Path
 
-# serial port of Arduino ("COM5" in Windows)
-arduino_port = "/dev/cu.SLAB_USBtoUART"
-baud = 115200  # arduino uno runs at 115200 baud
+# serial port of Arduino
+port = "COM5" if platform.system() == "Windows" else "/dev/cu.SLAB_USBtoUART"
+baud = 115200  # Arduino runs at 115200 baud
+
+# connect to Arduino
+ser = serial.Serial(port, baud)
+print("Connected toArduino port:" + port)
 
 # take datetime now in ISO 8061 format and make that the filename
 now = datetime.datetime.now()
 file_name = now.strftime("%Y-%m-%d-%H-%M-%S") + ".csv"
 
-# Path for the data file
+# path for the data file
 data_folder = Path("data/raw/")
 file_path = data_folder / file_name
 
-ser = serial.Serial(arduino_port, baud)
-print("Connected to Arduino port:" + arduino_port)
-
-samples = 2000  # how many samples to collect
+samples = 100  # how many samples to collect
 line = 0
-while line <= samples:
+
+while line < samples:
+
     get_data = str(ser.readline())
-    data = get_data  # ecg_timestamp,ecg,ppg1_timestamp,ppg1_ir,ppg1_red,ppg1_green,ppg2_timestamp,ppg2_ir,ppg2_red,ppg2_green
+
+    # remove extra characters from the data
+    data = re.sub('[brn\'\\\]', '', get_data)
     print(data)
-    if line > 10:  # Ignore Arduino info in the beginning
+
+    # ignore information printed in the first lines from the Arduino
+    if any(character.isalpha() for character in data):
+        pass
+    else:
         file = open(file_path, "a")
-        file.write(data[2:-5] + "\r\n")  # Ignore extra characters
-    line = line+1
+        # ecg_time, ecg, ppg1_time, ppg1_ir, ppg1_red, ppg1_green, ppg2_time, ppg2_ir, ppg2_red, ppg2_green
+        file.write(data + "\n")
+        line += 1
 
 print("Finished")
 file.close()
